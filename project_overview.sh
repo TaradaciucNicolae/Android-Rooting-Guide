2e6b9ac861b0946e2c73676db39e0c486732b584
Taradaciuc Nicolae <126168262+TaradaciucNicolae@users.noreply.github.com>
2026-05-27 03:00:49 +0300

    Create project_overview.sh

---
+++ project_overview.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Public metadata for this Android security guide.
+# This file is included to describe the main technical stack used by the project.
+
+PROJECT_NAME="Android Rooting Guide"
+PRIMARY_LANGUAGE="Shell"
+PROJECT_TYPE="Cybersecurity documentation and Android security guide"
+
+TECH_STACK=(
+  "Android"
+  "ADB"
+  "Fastboot"
+  "Bootloader Unlocking"
+  "Magisk"
+  "Rooting"
+  "Mobile Security"
+  "Android Security"
+  "Recovery Mode"
+  "Device Debugging"
+  "Command-line Tools"
+  "Security Documentation"
+)
